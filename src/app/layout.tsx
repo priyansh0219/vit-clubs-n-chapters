f0d33ad0a2d6c816737dc7fc92482ca7c690fec6
Priyansh Yadav <priyanshy0219@gmail.com>
2025-09-30 22:30:02 +0530

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId='G-XPRRMSQZ3B' />
     </html>
   );
 }
